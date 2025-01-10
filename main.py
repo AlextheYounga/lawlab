f1bc246d60d8decfc56842cbf2d6ddc12533aa32
Alex Younger <thealexyounger@proton.me>
2025-01-10 02:25:38 -0600

    rewrite: rewrite in rust

--- main.py
+++ main.py
@@ -26,53 +26,10 @@ def run_preflight(release_id):
 	# Recursively find the htm files
 	subprocess.run("./consistent_usc_files", shell=True)
 
-def handle_html_files():
-	  # Split HTML documents based on chapters and prune unnecessary tags from the content
-	export_files = os.listdir('storage/usc')
-	for file in export_files:
-		print(f'Processing {file}...')
-		split_html(file)
-		
-def handle_markdown_conversion():
-	# Convert HTML files to markdown
-	html_files = os.listdir('out/templates/html')
-	os.makedirs('out/templates/markdown', exist_ok=True)	
-	for file in html_files:
-		print(f'Processing {file}...')
-		convert_to_markdown(file)
-
-def handle_html_files_async():
-	# Split HTML documents based on chapters and prune unnecessary tags from the content
-	monitor_process = multiprocessing.Process(target=html_file_monitor)
-	monitor_process.start()
-	try:
-		export_files = os.listdir(f'storage/usc')
-		os.makedirs('out/templates/html', exist_ok=True)
-		with multiprocessing.Pool(PROCESSES) as pool:
-			for _ in pool.imap_unordered(split_html_async, export_files):
-				pass
-	except KeyboardInterrupt:
-		print('Exiting...')
-		monitor_process.kill()
-		exit(0)
-	print('Done processing HTML files')
-	monitor_process.kill()
-
-
-def handle_rust_markdown_conversion():
+def pass_to_rust_handler():
 	# Crazy fast
 	print('Handing off to Rust to convert to Markdown...')
 	return subprocess.run('./target/release/markdownconverter')
-
-
-def copy_to_out():
-	for mdfile in os.listdir(f'./out/templates/markdown'):
-		document_id = mdfile.replace('.md', '')
-		path_value = r.get(document_id)
-		file_path = path_value.decode('utf-8')
-		out_path = Path(f'out/usc/{file_path}.md')
-		os.makedirs(out_path.parent, exist_ok=True)
-		shutil.copyfile(f'out/templates/markdown/{mdfile}', out_path.__str__())  
 
 
 def cleanup():
@@ -109,10 +66,8 @@ def main():
 		# Preflight: unzip the usc files and remove previous output.
 		run_preflight(id)
 
-		# Handle file conversions.
-		handle_html_files_async()
-		handle_rust_markdown_conversion()
-		copy_to_out() # now this is slow
+		pass_to_rust_handler()
+
 		# handle_markdown_conversion_async()
 		end_time = time.time() # Time the main program
 		print(f'\nDone! Time taken: {end_time - time_start:.2f} seconds')
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::fs;
 use std::fs::File;
-use std::io::{self, Write};
+use std::path::PathBuf;
+use std::io::{ self, Write };
+use std::borrow::Cow;
 use html2md::parse_html;
 use rayon::prelude::*;
-use std::path::PathBuf;
-
+use scraper::{ Html, Selector, ElementRef };
+use regex::Regex;
 
 fn write_to_file(filepath: String, buffer: Vec<u8>) -> io::Result<()> {
     let mut file = File::create(filepath)?;
@@ -12,31 +14,153 @@ fn write_to_file(filepath: String, buffer: Vec<u8>) -> io::Result<()> {
     Ok(())
 }
 
-fn create_markdown_dir() -> io::Result<()> {
-	fs::create_dir_all("./out/templates/markdown")?;
+fn create_out_dirs(filepath: &String) -> io::Result<()> {
+    fs::create_dir_all(filepath)?;
     Ok(())
 }
 
+fn format_citation_comment(citation: &str) -> String {
+    return String::from(citation)
+        .to_lowercase()
+        .trim()
+        .replace(" ", "-")
+        .replace("expcite:", "")
+        .replace(".", "")
+        .replace("/", "")
+        .replace(",", "")
+        .replace("!@!", "/");
+}
+
+// fn handle_appendix(doc_path: PathBuf, doc_html: Html) {
+//     /* The correct title name is not found in the appendix file itself,
+// 	so I have to find the correct title document by removing the "a" after the title number.
+// 	 */
+//     let re: Regex = Regex::new(r"\d+a").unwrap();
+//     let doc_name: Cow<'_, str> = doc_path.to_string_lossy();
+//     let title_id = re.find(&doc_name).unwrap();
+//     let title_number = title_id.as_str().replace("a", "");
+//     let main_doc_location = doc_name.replace(title_id.as_str(), &title_number);
+//     let main_doc_bytes = fs
+//         ::read(main_doc_location)
+//         .expect("Should have been able to read the file");
+//     let main_doc_string = String::from_utf8_lossy(&main_doc_bytes);
+
+//     for line in main_doc_string.lines() {
+//         if line.contains("expcite") {
+//             let citation = format_citation_comment(line);
+//             let file_path = citation + "/appendix";
+//             // write to file
+//         }
+//     }
+// }
+
+fn find_section_path(section_element: ElementRef<'_>) -> String {
+	let mut section_path = String::new();
+    for element in section_element.ancestors() {
+        if element.value().is_comment() && element.value().as_text().is_some() {
+			if element.value().as_text().unwrap().contains("expcite") {
+				let item_citation: String = element.value().as_text().unwrap().to_string();
+				section_path = format_citation_comment(&item_citation);
+				break;
+			}
+        }
+    }
+	return section_path;
+}
+
+fn find_document_id(section_element: ElementRef<'_>) -> String {
+	let mut document_id = String::new();
+    for element in section_element.ancestors() {
+        if element.value().is_comment() && element.value().as_text().is_some() {
+            if element.value().as_text().unwrap().contains("documentid") {
+                let item_citation: String = element.value().as_text().unwrap().to_string();
+				document_id = item_citation
+					.split("documentid:")
+					.nth(1)
+					.unwrap()
+					.split(" ")
+					.nth(0)
+					.unwrap()
+					.trim()
+					.to_string();
+				break;
+            }
+        }
+    }
+	return document_id;
+}
+
 fn main() {
-    // Collect paths into a Vec<PathBuf>
-    let paths: Vec<PathBuf> = fs::read_dir("./out/templates/html")
+    let docs: Vec<PathBuf> = fs
+        ::read_dir("./storage/usc")
         .unwrap()
         .filter_map(|entry| entry.ok().map(|e| e.path()))
         .collect();
+
+    for doc_path in docs {
+		let doc_name: Cow<'_, str> = doc_path.to_string_lossy();
+		if doc_name.ends_with(".htm") {
+			let doc_bytes = fs::read(&doc_path).expect("Should have been able to read the file");
+			let doc_contents: Cow<'_, str> = String::from_utf8_lossy(&doc_bytes);
+			let doc_html: Html = Html::parse_document(&doc_contents);
+			let title_head_selector: Selector = Selector::parse("h1.usc-title-head").unwrap();
+			let title_element: ElementRef<'_> = doc_html.select(&title_head_selector).next().unwrap();
+			let title: String = String::from(title_element.inner_html().as_str());
+			println!("{}", title);
 	
-	if create_markdown_dir().is_ok() {
-		paths.par_iter().for_each(|filepath| {
-			let contents = fs::read_to_string(filepath).expect("Should have been able to read the file");
-			let markdown = parse_html(&contents);
-			let buffer = markdown.into_bytes();
-			let mdfilename = filepath
-				.to_string_lossy()
-				.replace("/html/", "/markdown/")
-				.replace(".html", ".md");
-	
-			if let Err(e) = write_to_file(mdfilename, buffer) {
-				eprintln!("Error writing to file: {}", e);
+			if title.contains("APPENDIX") {
+				continue;
+				// return handle_appendix(doc_path, doc_html);
 			}
-		});
+	
+			let section_selector = Selector::parse("h3.section-head").unwrap();
+			let mut section_elements = doc_html.select(&section_selector);
+			let mut sections = doc_contents.split("<h3 class=\"section-head\">");
+			// Pop the first value
+			let mut front_matter = "";
+			if let Some(first_value) = sections.next() {
+				front_matter = first_value;
+			}
+	
+			// Map out all the html we'll need before converting.
+			let mut section_vec: Vec<[String; 3]> = Vec::new();
+			let sections_count = sections.clone().count();
+			for (i, section) in sections.into_iter().enumerate() {
+				let section_element = section_elements.next().unwrap();
+				let mut section_content = section_element.html() + section;
+				if i == 0 {
+					section_content.insert_str(0, front_matter); // that's dumb...
+				}
+				if i == sections_count - 1 {
+					section_content.insert_str(0, "<html><body><div>");
+				}
+	
+				// Get item "citation" from document which contains the path.
+				let section_path = find_section_path(section_element);
+				let document_id = find_document_id(section_element);
+	
+				section_vec.push([document_id, section_path, section_content])
+			}
+	
+			// Run in parallel to convert to markdown
+			section_vec.par_iter().for_each(|doc_items| {
+				let doc_id = &doc_items[0];
+				let section_path = &doc_items[1];
+				let section_content = &doc_items[2];
+
+				// Convert to markdown
+            	let markdown = parse_html(&section_content);
+            	let buffer = markdown.into_bytes();
+				let out_path = String::from("out/usc/") + section_path + ".md";
+
+				if create_out_dirs(&out_path).is_ok() {
+					// println!("Would output {}", &out_path);
+					// if let Err(e) = write_to_file(mdfilename, buffer) {
+					// 	eprintln!("Error writing to file: {}", e);
+					// }
+				}
+
+			})
+		}
     }
 }
